f4a194098724be6fa911f4a67b6cb22484f45873
mariocfdias <82966851+mariocfdias@users.noreply.github.com>
2022-11-11 08:37:56 -0300

    Update receptor.ino

--- receptor/receptor.ino
+++ receptor/receptor.ino
@@ -1,8 +1,3 @@
-// ----------------------------------------------------------------------------
-// ESP-NOW network + WiFi gateway to the Internet
-// ----------------------------------------------------------------------------
-// ESP-NOW receiver acting as a WiFi gateway to the Internet
-// ----------------------------------------------------------------------------
 
 #include <Arduino.h>
 #include <WiFi.h>
@@ -23,10 +18,6 @@ float floatValue;
 bool signupOK = false;
 averager<int, 10> voltage;
 
-// ----------------------------------------------------------------------------
-// WiFi handling
-// ----------------------------------------------------------------------------
-
 constexpr char WIFI_SSID[] = "HUAWEIMario";
 constexpr char WIFI_PASS[] = "";
 
@@ -35,27 +26,24 @@ void initWiFi() {
     WiFi.mode(WIFI_MODE_APSTA);
     WiFi.begin(WIFI_SSID, WIFI_PASS);
 
-    Serial.printf("Connecting to %s .", WIFI_SSID);
+    Serial.printf("Conectando a rede %s .", WIFI_SSID);
     while (WiFi.status() != WL_CONNECTED) { Serial.print("."); delay(200); }
     Serial.println(" ok");
 
     IPAddress ip = WiFi.localIP();
 
     Serial.printf("SSID: %s\n", WIFI_SSID);
-    Serial.printf("Channel: %u\n", WiFi.channel());
+    Serial.printf("Canal: %u\n", WiFi.channel());
     Serial.printf("IP: %u.%u.%u.%u\n", ip & 0xff, (ip >> 8) & 0xff, (ip >> 16) & 0xff, ip >> 24);
 
   
 
 }
 
-// ----------------------------------------------------------------------------
-// ESP-NOW handling
-// ----------------------------------------------------------------------------
 
 void onReceive(const uint8_t *mac_addr, const uint8_t *data, int len) {
 
-    Serial.printf("received: %3u from %02x:%02x:%02x:%02x:%02x:%02x\n",
+    Serial.printf("Recebeu: %3u de %02x:%02x:%02x:%02x:%02x:%02x\n",
         (uint8_t) *data,
         mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]
     );
@@ -72,16 +60,13 @@ void onReceive(const uint8_t *mac_addr, const uint8_t *data, int len) {
 void initEspNow() {
 
     if (esp_now_init() != ESP_OK) {
-        Serial.println("ESP NOW failed to initialize");
+        Serial.println("ESP NOW falhou ao iniciar");
         while (1);
     }
 
     esp_now_register_recv_cb(onReceive);
 }
 
-// ----------------------------------------------------------------------------
-// Initialization
-// ----------------------------------------------------------------------------
 
 void setup() {
     Serial.begin(115200);
@@ -90,13 +75,10 @@ void setup() {
     initWiFi();
     initEspNow();
 
-        /* Assign the api key (required) */
   config.api_key = API_KEY;
 
-  /* Assign the RTDB URL (required) */
   config.database_url = DATABASE_URL;
 
-  /* Sign up */
   if (Firebase.signUp(&config, &auth, "", "")){
     Serial.println("ok");
     signupOK = true;
@@ -105,27 +87,21 @@ void setup() {
     Serial.printf("%s\n", config.signer.signupError.message.c_str());
   }
 
-  /* Assign the callback function for the long running token generation task */
-  config.token_status_callback = tokenStatusCallback; //see addons/TokenHelper.h
+  config.token_status_callback = tokenStatusCallback; // addons/TokenHelper.h
   
   Firebase.begin(&config, &auth);
   Firebase.reconnectWiFi(true);
 }
 
-// ----------------------------------------------------------------------------
-// Main control loop
-// ----------------------------------------------------------------------------
 
 void loop(){
      if(Firebase.ready()){
        if (Firebase.RTDB.pushInt(&fbdo, "painel/teste", voltage.getAverage())){
-      Serial.println("PASSED");
-      Serial.println("PATH: " + fbdo.dataPath());
-      Serial.println("TYPE: " + fbdo.dataType());
+      Serial.println("Caminho do dado: " + fbdo.dataPath());
+      Serial.println("Tipo: " + fbdo.dataType());
     }
     else {
-      Serial.println("FAILED");
-      Serial.println("REASON: " + fbdo.errorReason());
+      Serial.println("ERRO:: " + fbdo.errorReason());
     }
     }
 }
